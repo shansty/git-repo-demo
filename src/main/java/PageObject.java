bbad72153c72c3d3777b66966e8eb5fc998e0630
Anastasiya Shirochina <shirochina16@gmail.com>
2023-06-13 19:41:42 +0300

    task

--- src/main/java/PageObject.java
+++ src/main/java/PageObject.java
@@ -1,6 +1,8 @@
 public class PageObject {
     String enterButtonXpath;
     String passwordButtonXpath;
+    String a;
+    String b;
 
     public PageObject fillPassword(String password) {
         return this;
